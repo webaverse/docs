30e056b8801289b67b6a99b0ec3906614028bc90
Zetaphor <zetaphor@gmail.com>
2020-06-09 13:25:20 -0400

    Setup GH pages publishing

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 [https://docs.webaverse.com](https://docs.webaverse.com)
 
 Powered by Docusaurus
+
+## Pubishing
+
+```bash
+cd website
+USE_SSH=true yarn run publish-gh-pages
+```
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'Webaverse Documentation', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://docs.webaverse.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://webaverse.github.io', // Your website URL
+  baseUrl: '/docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
