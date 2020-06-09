ff4c7c7bb96e01fa2de7e31b9663cab0d57bde25
Zetaphor <zetaphor@gmail.com>
2020-06-09 13:45:06 -0400

    Add trailing slash to baseURL

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Webaverse Documentation', // Title for your website.
   tagline: 'Webaverse Documentation',
   url: 'https://docs.webaverse.com', // Your website URL
-  baseUrl: '/docs', // Base URL for your project */
+  baseUrl: '/docs/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
