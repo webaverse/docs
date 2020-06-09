69ccf2979896c739bdffb22e69bcdf92e3fd533d
Zetaphor <zetaphor@gmail.com>
2020-06-09 13:48:18 -0400

    Move CNAME entry to siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -32,6 +32,7 @@ const siteConfig = {
   // Used for publishing and more
   projectName: 'docs',
   organizationName: 'webaverse',
+  cname: 'docs.webaverse.com',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
